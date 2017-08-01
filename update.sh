d9f9b4b5498fce12b4f9749f72209f589590681c
Tibor Vass <teabee89@gmail.com>
2017-08-01 11:38:43 -0700

    Copy dependencies in vendor/
    Signed-off-by: Tibor Vass <teabee89@gmail.com>

--- update.sh
+++ update.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 BRANCH="moby-split-test-17.06" # would be branches like 17.06 etc.
 # branch that needs docker/docker be rewritten to moby/moby-core. Following was used on docker-ce:
 # 
 # git grep -l "github.com/docker/docker" | xargs sed -i '' -E 's,github.com/docker/docker(["/ ]|$),github.com/moby/moby-core\1,g' && git status -s | cut -d' ' -f3- | grep '.*\.go$' | xargs gofmt -w -s
 
+sed=$(which gsed) || sed=$(which sed)
 dir=$(pwd)
 tmp=$(mktemp -d)
 (
@@ -13,11 +14,23 @@ tmp=$(mktemp -d)
 	rm -rf "$dir"/go/mobycore
 	mkdir -p "$dir"/go
 	cp -r docker-ce/components/engine/client "$dir"/go/mobycore
-	find "$dir"/go -name '*.go' -print | xargs sed -i '' -E 's,\bpackage client\b,package mobycore,g'
-	#for rule in \
-	#	'"github.com/docker/docker/client" -> "github.com/tiborvass/devkit/go/mobycore"' \
-	#	; do
-	#	gofmt -w -r "$rule" "$dir"/go/mobycore/*.go
-	#done
+	cd "$dir"/go/mobycore
+
+	find . -name '*.go' -print | xargs $sed -i'' -E 's,\b([pP])ackage client\b,\1ackage mobycore,g'
+
+	deps=$(go list -json ./... | jq -r '[.Imports[],.TestImports[]][]' | sort -u | grep -E '^[^/]+\.[^/]+')
+	mobycorePrefix='github.com/moby/moby-core/'
+	for dep in $deps; do
+		if echo "$dep" | grep -q "^$mobycorePrefix"; then
+			scopedImportPath="${dep:${#mobycorePrefix}}"
+			dest="vendor/${mobycorePrefix}${scopedImportPath}"
+			mkdir -p $(dirname "$dest")
+			cp -r "$tmp/docker-ce/components/engine/${scopedImportPath}" "$dest"
+		else
+			dest="vendor/$dep"
+			mkdir -p $(dirname "$dest")
+			cp -r "$tmp/docker-ce/components/engine/vendor/$dep" "$dest"
+		fi
+	done
 )
 rm -rf "$tmp"
