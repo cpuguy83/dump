6eeb013bf9d4d5c534b9dfbd6b860cc5832c9e7a
Brian Goff <cpuguy83@gmail.com>
2017-08-01 17:28:27 -0400

    Recurse moby deps to ensure all are vendored
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- update.sh
+++ update.sh
@@ -5,9 +5,41 @@ BRANCH="moby-split-test-17.06" # would be branches like 17.06 etc.
 # 
 # git grep -l "github.com/docker/docker" | xargs sed -i '' -E 's,github.com/docker/docker(["/ ]|$),github.com/moby/moby-core\1,g' && git status -s | cut -d' ' -f3- | grep '.*\.go$' | xargs gofmt -w -s
 
+
+mobycorePrefix='github.com/moby/moby-core/'
+
 sed=$(which gsed) || sed=$(which sed)
 dir=$(pwd)
 tmp=$(mktemp -d)
+
+importMobyDep() {
+    local base=${1}
+    local dep="${2}"
+
+
+    scopedImportPath="${dep:${#mobycorePrefix}}"
+    dest="vendor/${mobycorePrefix}${scopedImportPath}"
+    mkdir -p $(dirname "$dest")
+    cp -r "${base}/docker-ce/components/engine/${scopedImportPath}" "$dest"
+
+    deps=$(goList "./${dest}")
+    for dep in $deps; do
+        if isMobyDep "$dep"; then
+            importMobyDep "$base" "$dep"
+        fi
+    done
+}
+
+goList() {
+    local path="$1"
+    go list -json $path | jq -r '[.Imports[],.TestImports[]][]' | sort -u | grep -E '^[^/]+\.[^/]+'
+}
+
+isMobyDep() {
+    local dep=$1
+    echo "$dep" | grep -q "^$mobycorePrefix";
+}
+
 (
 	cd "$tmp"
 	git clone --depth 1 -b "$BRANCH" https://github.com/tiborvass/docker-ce
@@ -18,19 +50,16 @@ tmp=$(mktemp -d)
 
 	find . -name '*.go' -print | xargs $sed -i'' -E 's,\b([pP])ackage client\b,\1ackage mobycore,g'
 
-	deps=$(go list -json ./... | jq -r '[.Imports[],.TestImports[]][]' | sort -u | grep -E '^[^/]+\.[^/]+')
-	mobycorePrefix='github.com/moby/moby-core/'
-	for dep in $deps; do
-		if echo "$dep" | grep -q "^$mobycorePrefix"; then
-			scopedImportPath="${dep:${#mobycorePrefix}}"
-			dest="vendor/${mobycorePrefix}${scopedImportPath}"
-			mkdir -p $(dirname "$dest")
-			cp -r "$tmp/docker-ce/components/engine/${scopedImportPath}" "$dest"
-		else
-			dest="vendor/$dep"
-			mkdir -p $(dirname "$dest")
-			cp -r "$tmp/docker-ce/components/engine/vendor/$dep" "$dest"
-		fi
-	done
+    deps="$(goList ./...)"
+    for dep in $deps; do
+        if isMobyDep "$dep"; then
+            importMobyDep "$tmp" "$dep"
+        else
+            dest="vendor/$dep"
+            mkdir -p $(dirname "$dest")
+            cp -r "$tmp/docker-ce/components/engine/vendor/$dep" "$dest"
+        fi
+    done
 )
+
 rm -rf "$tmp"
